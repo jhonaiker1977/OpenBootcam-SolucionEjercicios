eba9fb3c1c53c2c6c62978b439d86caedd7934b6
jhonaiker1977 <109928860+jhonaiker1977@users.noreply.github.com>
2023-01-16 20:25:44 -0400

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+public class Main {
+    public static void main(String[] args) {
+
+       // Condicional if
+
+        int numeroif = 3;
+
+        if (numeroif >= 0){
+            System.out.println("Es Positivo");
+        }else if (numeroif <= 0){
+            System.out.println("Es Negativo");
+
+        }
+
+        // Condicional while
+
+        int numeroWhile = 1;
+
+        while (numeroWhile <= 8){
+            System.out.println(numeroWhile);
+            numeroWhile+= 1;
+        }
+
+        // Condicional Do While
+
+        int i = 1;
+
+        do {
+            System.out.println(i);
+            i+=1;
+        }while (i>=8);
+
+        // Condicional For
+
+        int numeroFor= 0;
+        for(int NumeroFor=0 ; numeroFor<=3 ; numeroFor++){
+            System.out.println(numeroFor);
+        }
+
+        // Condicional Switch
+
+        var estacion = "Invierno";
+
+        switch (estacion){
+            case "Verano":
+            System.out.println("Es Verano");
+            break;
+            case "Primavera":
+            System.out.println("Es Primavera");
+            break;
+            case "Otoño":
+            System.out.println("Es Otoño");
+            break;
+            case "Invierno":
+            System.out.println("Es Invierno");
+            break;
+            default:
+            System.out.println("Ninguna de las anteriores");
+
+
+        }
+
+}
+}

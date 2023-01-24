12cf853ff74a6bc3513299ff7fb617f23b43a8b6
jhonaiker1977 <109928860+jhonaiker1977@users.noreply.github.com>
2023-01-24 00:51:23 -0400

    Add files via upload

---
+++ Introduccion a la Programacion/Tema_8AstraccionyEncasulacion/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[]args){
+        Persona persona1 = new Persona();
+
+        persona1.setEdad(46);
+        persona1.setNombre("Carlos");
+        persona1.setTelefono(654465165);
+
+        System.out.println(persona1.getEdad());
+        System.out.println(persona1.getNombre());
+        System.out.println(persona1.getTelefono());
+
+    }
+    
+}
+
+
+
+
+
+
+

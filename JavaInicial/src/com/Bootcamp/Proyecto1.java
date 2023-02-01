cafe44dbf43829e46ccfdaebc510c1467f657da9
jhonaiker1977 <109928860+jhonaiker1977@users.noreply.github.com>
2023-02-01 10:18:31 -0400

    Add files via upload

---
+++ JavaInicial/src/com/Bootcamp/Proyecto1.java
@@ -0,0 +1,50 @@
+package com.Bootcamp;
+
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+
+        //Tipos de Datos:
+
+        //Enteros:
+        byte numero1 =1;
+        short numero2 =2;
+        int numero3 =3;
+        long numero4 =4;
+
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(numero4);
+
+        //Punto flotante:
+        float numero5 =5.0f;
+        double numero6 =6.0;
+
+        System.out.println(numero5);
+        System.out.println(numero6);
+
+        //Booleano:
+        boolean numero7 =true;
+        boolean numero8 =false;
+
+        System.out.println(numero7);
+        System.out.println(numero8);
+
+        //Cadenas:
+        String cadena1 = "Hola";
+        String cadena2 = "Mundo";
+
+        System.out.println(cadena1);
+        System.out.println(cadena2);
+
+
+        //caracter
+        char caracter1 = 'a';
+        char caracter2 = 'b';
+
+            System.out.println(caracter1);
+            System.out.println(caracter2);
+            
+    }
+}

d30957236928c08ee432c20b61cc00598d140962
jhonaiker1977 <109928860+jhonaiker1977@users.noreply.github.com>
2023-02-09 22:15:15 -0400

    Add files via upload

---
+++ JavaInicial/EstructuraDeControl/src/Bucles.java
@@ -0,0 +1,11 @@
+public class Bucles {
+    public static void main(String[] args) {
+
+        String [] nombres = {"Carlos", "Manuelos", "Marias", "Pedro"};
+        String [] apellidos = {"Perez", "Mejias", "Ruiz", "Arez"};
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i] + " " + apellidos[i]);
+        }
+    }
+}
